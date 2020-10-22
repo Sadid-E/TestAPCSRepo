fdab0aa1e09cc6e5c50d2b21cd391a52fb17d81e
Sadid-E <sethun20@stuy.edu>
2020-10-21 20:41:45 -0400

    arrayDeepToString is working

--- Demo.java
+++ Demo.java
@@ -5,15 +5,30 @@ public static void main(String[] args) {
     printLoop(5);
   } else
   printLoop(Integer.parseInt(args[0]));
-  }
-
-public static void printLoop(int n) {
-  for (int i = 1; i <= n; i++) {
-    for (int j = i; j <= n; j++) {
-      System.out.print(i);
-    }
-    System.out.println();
-  }
-
 }
+
+public static String arrToString(int[] arr){
+  String str = "{";
+  if (arr.length == 0) {
+    return "{}";
+  }
+  for (int i = 0; i <arr.length - 1; i++) {
+    str += + arr[i] + ", ";
+  }
+  str += arr[arr.length-1] + "}";
+  return str;
+}
+
+public static String arrayDeepToString(int[][] arr) {
+  String str = "{";
+  if (arr.length == 0) {
+    return "{}";
+  }
+  for (int i = 0; i < arr.length - 1; i++) {
+    str += arrToString(arr[i]) + ", ";
+  }
+  str += arrToString(arr[arr.length-1]) + "}";
+  return str;
+}
+
 }
